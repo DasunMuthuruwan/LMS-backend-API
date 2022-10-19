447d44ac9be243cc1d5f7912644e6c36a5a1c694
dasun94 <dasun@mylinex.com>
2022-10-19 22:39:09 +0530

    Setup Server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+// config dot env
+import  'dotenv/config'
+import express from "express"
+import cors from 'cors'
+
+// config express
+const app = express()
+const PORT = process.env.PORT || 8090
+
+// set middleware
+app.use(express.json({limit: "20mb"}))
+// access frontend url only retrict another url (we can add array url)
+// app.use(cors({origin: "http://localhost:3000"}))
+app.use(cors())
+
+app.get('/', (req, res, next) => {
+    res.send('Hello')
+    next()
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on ${PORT}`);
+})
